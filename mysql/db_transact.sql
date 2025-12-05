becf41aa8307a412a44876895a3c135e62bcc630
ivanmca <ivanmcaa@gmail.com>
2025-12-05 00:41:47 +0000

    Actualizar el código ETL, corregir errores tipográficos en requerimientos, modificar la estructura de la base de datos y agregar configuración de VSCode.

--- mysql/db_transact.sql
+++ mysql/db_transact.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE res_province_transact;
+CREATE DATABASE res_province_transact_db;
 
-USE res_province_transact;
+USE res_province_transact_db;
 
 CREATE TABLE res_province (
     id INTEGER PRIMARY KEY NOT NULL,    
@@ -8,3 +8,17 @@ CREATE TABLE res_province (
 );
 
 INSERT INTO res_province (id, name) VALUES (1,'La Paz'),(2,'Santa Cruz'),(3,'Tarija');
+
+CREATE TABLE street (
+    id INTEGER PRIMARY KEY NOT NULL,    
+    res_province_id INTEGER REFERENCES res_province(id),
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO street (id, res_province_id, name) VALUES 
+(1,1,'Avenida Argentina'),
+(2,1,'Calle Colon'),
+(3,2,'Avenida Monseñor Rivero'),
+(4,2,'Calle La Salle'),
+(5,3,'Avenida Las Américas'),
+(6,3,'Calle Cochabamba');
