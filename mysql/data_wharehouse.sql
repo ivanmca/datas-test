6dcc77b4ae67bf4e9a8db1ad8b9617d9f0b7035d
ivanmca <ivanmcaa@gmail.com>
2025-12-05 02:11:54 +0000

    Agregar tablas res_country y res_street en el script de creación de base de datos

--- mysql/data_wharehouse.sql
+++ mysql/data_wharehouse.sql
@@ -8,3 +8,13 @@ CREATE TABLE res_country (
     code VARCHAR(8) NOT NULL,
     name VARCHAR(100)
 );
+
+CREATE TABLE res_street (
+    id INTEGER PRIMARY KEY NOT NULL,
+    country_id INTEGER,
+    phone_code INTEGER,
+    code VARCHAR(8),
+    country VARCHAR(255),
+    province VARCHAR(255),
+    street_name VARCHAR(255)        
+);
